4765d15312a893f6a6281346a7dbefd534e658c0
Joseph Maxwell (Hyland) <106829895+josephmaxwellhyland@users.noreply.github.com>
2022-06-03 12:58:48 -0400

    fix uri exposure

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://admin:FhpTqCMwIEHYjb5k@cluster0.mfwj72d.mongodb.net/?retryWrites=true&w=majority",
+  mongoURI: "URI_HERE",
   secretOrKey: "secret"
 };
